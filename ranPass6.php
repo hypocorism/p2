870964f7e8cb3fbf5ef43d7e2c13e7577ffc0c45
hypocorism <hypocorism@users.noreply.github.com>
2014-10-17 17:21:48 -0400

    Corrected title & eliminated error messages

--- ranPass6.php
+++ ranPass6.php
@@ -5,18 +5,14 @@
 	<link rel='stylesheet' href='css/styles.css' type='text/css'>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>XKVCD Human Password Generator</title>
+	<title>XKCD Human Password Generator</title>
     <script src="js/vendor/modernizr.js"></script>
 </head>
 
-<?php
-error_reporting(E_ALL);	#Report Errors, Warnings, and Notices
-ini_set('display_errors', 1);	#Display errors on page 
-?>
 	<body>
 		
 		<h1>
-			XKVCD Human Password Generator:
+			XKCD Human Password Generator:
 
 		</h1>
 		
